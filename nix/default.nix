127786a7f5586b37e0f2f4f84f50719e3837dce2
Andreas Rammhold <andreas@rammhold.de>
2019-10-18 12:25:36 +0200

    Update libnixstore-c to get a newer version of nix 2.3.1 with out patch applied

--- nix/default.nix
+++ nix/default.nix
@@ -10,8 +10,8 @@ let
          libnixstore-c = super.callPackage (super.fetchFromGitHub {
            owner = "andir";
            repo = "libnixstore-c";
-           rev = "fa0bb095033cff87e30b9ed6a1e659fa248b867e";
-           sha256 = "042afj357j2bq9y0nlcx1ps8g32yg00j4y9fn6fcvd70zrcsv4dk";
+           rev = "b0b1bc6da463c5c65dc1335e44dd60b98a65134d";
+           sha256 = "1nllgp0a66av6ji4irfw4ir414lrlx3v725jxyqxwx8krkcch1j5";
          }) {};
          nix = self.libnixstore-c.nix;
        })
