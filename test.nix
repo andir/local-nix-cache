daccf3124281f9c6bf7d201c3ee6bed47efe0bc1
Jörg Thalheim <joerg@thalheim.io>
2020-10-05 22:52:35 +0200

    update nixos test

--- test.nix
+++ test.nix
@@ -1,18 +1,20 @@
+{ pkgsSrc ? ./nix }:
+
 let
-  pkgs = import <nixpkgs> {};
+  pkgs = import pkgsSrc {};
   drvs = (import (fetchGit ./.) { pkgsSrc = pkgs.path; });
   module = import (drvs.path + "/module.nix");
 in
-(import (pkgs.path + "/nixos/tests/make-test.nix") ({ pkgs, lib, ...}: {
+(import (pkgs.path + "/nixos/tests/make-test-python.nix") ({ pkgs, lib, ...}: {
   machine = { pkgs, ... }: {
     imports = [ module ];
     local-nix-cache.server.enable = true;
   };
 
   testScript = ''
-    startAll;
-    $machine->waitForUnit('multi-user.target');
-    $machine->waitForUnit('local-nix-cache');
-    $machine->succeed('curl localhost:8083 -q');
+    start_all()
+    machine.wait_for_unit("multi-user.target")
+    machine.wait_for_unit("local-nix-cache")
+    machine.succeed("curl localhost:8083 -q")
   '';
 }))
